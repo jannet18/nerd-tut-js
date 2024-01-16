2ef90ad5ab14abaddd856f4a9ee16456932f8393
jannet18 <janetngei18@gmail.com>
2024-01-16 20:28:29 +0300

    best practice update

--- basic-converter/index.js
+++ basic-converter/index.js
@@ -1,23 +1,26 @@
-const formSubmit = document.getElementById("convert");
-// get elements
-// get input, prevent normal form al submissiom
-// convert to 3 decimals
-// confirm if in put is a number
-// if not diplay error
-formSubmit.addEventListener("submit", function (e) {
-  e.preventDefault();
-  // parseFloat- function used to parse a string and convert it to a floating-point Number.
-  let inputNum = parseFloat(document.getElementById("distance").value);
-  let answer = document.getElementById("invisible");
+(function () {
+  "use strict";
+  const formSubmit = document.getElementById("convert");
+  // get elements
+  // get input, prevent normal form al submissiom
+  // convert to 3 decimals
+  // confirm if in put is a number
+  // if not diplay error
+  formSubmit.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // parseFloat- function used to parse a string and convert it to a floating-point Number.
+    let inputNum = parseFloat(document.getElementById("distance").value);
+    let answer = document.getElementById("invisible");
 
-  //   alert(conversion);
-  if (inputNum) {
-    // toFixed - used to format a number and represent it as a string with a specified number of digits after the decimal Point
-    let conversion = (inputNum * 1.609344).toFixed(3);
+    //   alert(conversion);
+    if (inputNum) {
+      // toFixed - used to format a number and represent it as a string with a specified number of digits after the decimal Point
+      let conversion = (inputNum * 1.609344).toFixed(3);
 
-    // conversion = ((Math.round(conversion) * 1000) / 1000).toFixed(3);
-    answer.innerHTML = `<h2>${inputNum} miles is equivalent to ${conversion} kilometers</h2>`;
-  } else {
-    answer.innerHTML = "<h2>Please input a number!</h2>";
-  }
-});
+      // conversion = ((Math.round(conversion) * 1000) / 1000).toFixed(3);
+      answer.innerHTML = `<h2>${inputNum} miles is equivalent to ${conversion} kilometers</h2>`;
+    } else {
+      answer.innerHTML = "<h2>Please input a number!</h2>";
+    }
+  });
+})();
