152a1a865a61a8f30f01a8f3d9546451476ab0c0
jannet18 <janetngei18@gmail.com>
2024-01-16 18:25:24 +0300

    module intro

--- slideshow/index.js
+++ slideshow/index.js
@@ -1,25 +1,40 @@
 (function () {
   "use strict";
-  const myPhotos = ["banner.png", "shirt.jpeg", "one.jpg"];
+  const myPhotos = ["banner.png", "shirt.jpeg", "brown.jpeg"];
   let currentImage = 0;
 
   const container = document.getElementById("content");
   const nextPhoto = document.getElementById("next");
-  const prevPhoto = document.getElementById("prev");
+  const prevPhoto = document.getElementById("previous");
 
+  function swapImage() {
+    // create a new image element
+    const newSlide = document.createElement("img");
+    // set source attributes
+    newSlide.src = `slides/${myPhotos[currentImage]}`;
+    // set the class name
+    newSlide.className = "fadeinimg";
+    // append it to the container
+    container.appendChild(newSlide);
+    if (container.children.length > 2) {
+      container.removeChild(container.children[0]);
+    }
+  }
   nextPhoto.addEventListener("click", function (e) {
     e.preventDefault();
     currentImage++;
     if (currentImage > myPhotos.length - 1) {
       currentImage = 0;
     }
-    // create a new image element
-    let newSlide = document.createElement("img");
-    // set source attributes
-    newSlide.src = `slides ${myPhotos[currentImage]}`;
-    // set the class name
-    newSlide.className = "fadeinimg";
-    // append it to the container
-    container.appendChild(newSlide);
+    swapImage();
   });
-});
+
+  prevPhoto.addEventListener("click", function (e) {
+    e.preventDefault();
+    currentImage--;
+    if (currentImage < 0) {
+      currentImage = myPhotos.length - 1;
+    }
+    swapImage();
+  });
+})();
