2d3cc274910c6df594bf06374e62d49adef698ab
jannet18 <janetngei18@gmail.com>
2024-02-24 12:25:47 +0300

    debugging typescript

--- Modern-full-stack/typescript/dist/app.js
+++ Modern-full-stack/typescript/dist/app.js
@@ -1,6 +1,19 @@
 "use strict";
-function sayHi(humanName) {
-    alert(`Hello ${humanName}`);
+let employee = {
+    id: 1,
+    name: "Janet",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+function kgToLbs(weight) {
+    if (typeof weight === "number") {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
 }
-sayHi("Janet");
-let age = 20;
+kgToLbs("10Kg");
+kgToLbs(10);
+//# sourceMappingURL=app.js.map
--- Modern-full-stack/typescript/src/app.ts
+++ Modern-full-stack/typescript/src/app.ts
@@ -1,6 +1,54 @@
-function sayHi(humanName: string) {
-  alert(`Hello ${humanName}`);
-}
-sayHi("Janet");
+// function sayHi(humanName: string) {
+//   alert(`Hello ${humanName}`);
+// }
+// sayHi("Janet");
 
-let age: number = 20;
+// let age: number = 20;
+// if (age < 50) age += 10;
+// console.log(age);
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+let employee: Employee = {
+  id: 1,
+  name: "Janet",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+// union types
+function kgToLbs(weight: number | string): number {
+  // type narrowing
+  if (typeof weight === "number") {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
+  }
+}
+kgToLbs("10Kg");
+kgToLbs(10);
+
+// type intersection
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+// literal types (exact ,specific value)
+type Quantity = 15 | 100;
+let quantity: Quantity = 100;
+type Metric = "cm" | "inch";
